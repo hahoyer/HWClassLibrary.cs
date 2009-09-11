a602a8d333ae447a9a44e5744e04bd3002b14e10
hh <hh@c0adb3ac-85ad-b347-9a16-a660ac379d24>
2009-09-11 02:33:27 +0000

    git-svn-id: svn://svn.hahoyer.dyndns.org/migrep1/trunk/Framework/src/HWClassLibrary@4415 c0adb3ac-85ad-b347-9a16-a660ac379d24

--- Debug/Profiler.cs
+++ Debug/Profiler.cs
@@ -22,18 +22,18 @@ namespace HWClassLibrary.Debug
                 _count = count;
                 _data = profiler._profileItems.Values.OrderByDescending(x => x.Duration).ToArray();
                 _sumAll = _data.Sum(x => x.Duration);
-                _sumMax = new TimeSpan((long) (_sumAll.Ticks*(1.0 - hidden)));
+                _sumMax = new TimeSpan((long)(_sumAll.Ticks * (1.0 - hidden)));
                 _index = 0;
                 _sum = new TimeSpan();
             }
 
             public string Format()
             {
-                if(_data.Length == 0)
+                if (_data.Length == 0)
                     return "\n=========== Profile empty ============\n";
 
                 var result = "";
-                for(; _index < _data.Length && _index != _count && _sum <= _sumMax; _index++)
+                for (; _index < _data.Length && _index != _count && _sum <= _sumMax; _index++)
                 {
                     var item = _data[_index];
                     result += item.Format(_index.ToString());
@@ -48,49 +48,118 @@ namespace HWClassLibrary.Debug
 
                 result = "\n=========== Profile ==================\n" + stringAligner.Format(result);
                 result += "Total:\t" + _sumAll.Format3Digits();
-                if(_index < _data.Length)
-                    result += 
-                        " (" 
-                        + (_data.Length - _index) 
-                        + " not-shown-items " 
-                        + (_sumAll - _sum).Format3Digits() 
+                if (_index < _data.Length)
+                    result +=
+                        " ("
+                        + (_data.Length - _index)
+                        + " not-shown-items "
+                        + (_sumAll - _sum).Format3Digits()
                         + ")";
                 result += "\n======================================\n";
                 return result;
             }
         }
 
+        /// <summary>
+        /// Measures the specified expression.
+        /// </summary>
+        /// <typeparam name="T">The type the specitied expression returns</typeparam>
+        /// <param name="expression">a function without parameters returning something.</param>
+        /// <returns>the result of the invokation of the specified expression</returns>
         public static T Measure<T>(Func<T> expression)
         {
             return _instance.InternalMeasure("", expression, 1);
         }
 
+        /// <summary>
+        /// Measures the specified action.
+        /// </summary>
+        /// <param name="action">The action.</param>
         public static void Measure(Action action)
         {
             _instance.InternalMeasure("", action, 1);
         }
 
+        /// <summary>
+        /// Measures the specified expression.
+        /// </summary>
+        /// <typeparam name="T">The type the specitied expression returns</typeparam>
+        /// <param name="flag">A flag that is used in dump.</param>
+        /// <param name="expression">a function without parameters returning something.</param>
+        /// <returns>the result of the invokation of the specified expression</returns>
         public static T Measure<T>(string flag, Func<T> expression)
         {
             return _instance.InternalMeasure(flag, expression, 1);
         }
 
+        /// <summary>
+        /// Measures the specified action.
+        /// </summary>
+        /// <param name="flag">A flag that is used in dump.</param>
+        /// <param name="action">The action.</param>
         public static void Measure(string flag, Action action)
         {
             _instance.InternalMeasure(flag, action, 1);
         }
 
+        /// <summary>
+        /// Resets the profiler data.
+        /// </summary>
         public static void Reset()
         {
-            lock(_instance)
+            lock (_instance)
                 _instance.InternalReset();
             _instance = new Profiler();
         }
 
-        public static string Dump()
+        /// <summary>
+        /// Formats the data accumulated so far.
+        /// Same as <see cref="Format(System.Nullable{int},double)">Format(null, 0.1)</see>
+        /// </summary>
+        /// <returns>The formatted data</returns>
+        /// <remarks>
+        /// The result contains one line for each measured expression, that is not ignored.
+        /// Each line contains
+        /// <para>- the file path, the line and the start column of the measuered expression in the source file,
+        /// (The information is formatted in a way, that within VisualStudio doubleclicking on such a line will open it.) </para>
+        /// <para>- the flag, if provided,</para>
+        /// <para>- the ranking,</para>
+        /// <para>- the execution count,</para>
+        /// <para>- the average duration of one execution,</para>
+        /// <para>- the duration,</para>
+        /// of the expression.
+        /// The the lines are sorted by descending duration.
+        /// The expressions, that consumes less than 10 percent are not shown.
+        /// </remarks>
+        public static string Format()
         {
-            lock(_instance)
-                return new Dumper(_instance, null, 0.1).Format();
+            return Format(null, 0.1);
+        }
+
+        /// <summary>
+        /// Formats the data accumulated so far.
+        /// </summary>
+        /// <param name="count">The number of measured expressions in result.</param>
+        /// <param name="hidden">The relative amount of time that will be hidden in result.</param>
+        /// <returns>The formatted data.</returns>
+        /// <remarks>
+        /// The result contains one line for each measured expression, that is not ignored.
+        /// Each line contains
+        /// <para>- the file path, the line and the start column of the measuered expression in the source file,
+        /// (The information is formatted in a way, that within VisualStudio doubleclicking on such a line will open it.) </para>
+        /// <para>- the flag, if provided,</para>
+        /// <para>- the ranking,</para>
+        /// <para>- the execution count,</para>
+        /// <para>- the average duration of one execution,</para>
+        /// <para>- the duration,</para>
+        /// of the expression.
+        /// The the lines are sorted by descending duration.
+        /// by use of <paramref name="count"/> and <paramref name="hidden"/> the number of lines can be restricted.
+        /// </remarks>
+        public static string Format(int? count, double hidden)
+        {
+            lock (_instance)
+                return new Dumper(_instance, count, hidden).Format();
         }
 
         private static Profiler _instance = new Profiler();
@@ -126,14 +195,14 @@ namespace HWClassLibrary.Debug
 
         private void BeforeAction(string flag, int depth)
         {
-            lock(this)
+            lock (this)
             {
                 _stopwatch.Stop();
                 var start = _stopwatch.Elapsed;
                 _current.Suspend(start);
                 _stack.Push(_current);
-                var position = Tracer.MethodHeader(depth + 1)+flag;
-                if(!_profileItems.TryGetValue(position, out _current))
+                var position = Tracer.MethodHeader(depth + 1) + flag;
+                if (!_profileItems.TryGetValue(position, out _current))
                 {
                     _current = new ProfileItem(position);
                     _profileItems.Add(position, _current);
@@ -145,7 +214,7 @@ namespace HWClassLibrary.Debug
 
         private void AfterAction()
         {
-            lock(this)
+            lock (this)
             {
                 _stopwatch.Stop();
                 var end = _stopwatch.Elapsed;
@@ -176,15 +245,15 @@ namespace HWClassLibrary.Debug
         }
 
         public TimeSpan Duration { get { return _duration; } }
-        public TimeSpan AverageDuration { get { return new TimeSpan(_duration.Ticks/_countEnd); } }
+        private TimeSpan AverageDuration { get { return new TimeSpan(_duration.Ticks / _countEnd); } }
 
-        public bool IsValid { get { return _countStart == _countEnd && _suspendCount == 0; } }
+        private bool IsValid { get { return _countStart == _countEnd && _suspendCount == 0; } }
 
         public void Start(TimeSpan duration)
         {
             _countStart++;
             _duration -= duration;
-            if(IsValid)
+            if (IsValid)
                 Tracer.Assert(_duration.Ticks >= 0);
         }
 
@@ -192,7 +261,7 @@ namespace HWClassLibrary.Debug
         {
             _countEnd++;
             _duration += duration;
-            if(IsValid)
+            if (IsValid)
                 Tracer.Assert(_duration.Ticks >= 0);
         }
 
@@ -215,7 +284,7 @@ namespace HWClassLibrary.Debug
         {
             _suspendCount++;
             _duration += start;
-            if(IsValid)
+            if (IsValid)
                 Tracer.Assert(_duration.Ticks >= 0);
         }
 
@@ -223,7 +292,7 @@ namespace HWClassLibrary.Debug
         {
             _suspendCount--;
             _duration -= end;
-            if(IsValid)
+            if (IsValid)
                 Tracer.Assert(_duration.Ticks >= 0);
         }
     }
