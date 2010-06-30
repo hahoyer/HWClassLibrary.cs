e20f4f6f510d2dead9096c54b2c0a4869d403f1d
hh <hh@c0adb3ac-85ad-b347-9a16-a660ac379d24>
2010-06-30 07:26:01 +0000

    Unittests: stuctural
    git-svn-id: svn://svn.hahoyer.dyndns.org/migrep1/trunk/Framework/src/HWClassLibrary@4536 c0adb3ac-85ad-b347-9a16-a660ac379d24

--- UnitTest/CategoryAttribute.cs
+++ UnitTest/CategoryAttribute.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using JetBrains.Annotations;
-using HWClassLibrary.Helper;
 
 namespace HWClassLibrary.UnitTest
 {
@@ -35,59 +31,5 @@ namespace HWClassLibrary.UnitTest
     [AttributeUsage(AttributeTargets.Method)]
     public class ExplicitAttribute : Attribute
     {
-    }
-
-
-
-    public static class TestExtender
-    {
-        public static void RunTests(this Assembly rootAssembly)
-        {
-            var x = GetUnitTests(rootAssembly);
-            foreach(var type in x)
-                RunTests(type);
-        }
-
-        private static void RunTests(MethodInfo methodInfo)
-        {
-        }
-
-        private static IEnumerable<MethodInfo> GetUnitTests(Assembly rootAssembly)
-        {
-            return GetAssemblies(rootAssembly)
-                .SelectMany(assembly => assembly.GetTypes())
-                .Where(type => type.GetAttribute<TestFixtureAttribute>(true) != null)
-                .SelectMany(type=>type.GetMethods())
-                .Where(methodInfo => methodInfo.GetAttribute<TestAttribute>(true) != null);
-        }
-
-        private static IEnumerable<Assembly> GetAssemblies(Assembly rootAssembly)
-        {
-            var result = new[] {rootAssembly};
-            for(var referencedAssemblies = result;
-                referencedAssemblies.Length > 0;
-                result = result.Union(referencedAssemblies).ToArray())
-            {
-                referencedAssemblies = referencedAssemblies
-                    .SelectMany(assembly => assembly.GetReferencedAssemblies())
-                    .Select(AssemblyLoad)
-                    .Distinct()
-                    .Where(assembly => !result.Contains(assembly))
-                    .ToArray();
-            }
-            return result;
-        }
-
-        private static Assembly AssemblyLoad(AssemblyName yy)
-        {
-            try
-            {
-                return AppDomain.CurrentDomain.Load(yy);
-            }
-            catch(Exception e)
-            {
-                return Assembly.GetExecutingAssembly();
-            }
-        }
     }
 }
---
+++ UnitTest/TestExtender.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HWClassLibrary.Helper;
+
+namespace HWClassLibrary.UnitTest
+{
+    public static class TestExtender
+    {
+        public static void RunTests(this Assembly rootAssembly)
+        {
+            var x = GetUnitTests(rootAssembly);
+            foreach(var type in x)
+                RunTests(type);
+        }
+
+        private static void RunTests(MethodInfo methodInfo)
+        {
+        }
+
+        private static IEnumerable<MethodInfo> GetUnitTests(Assembly rootAssembly)
+        {
+            return GetAssemblies(rootAssembly)
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => type.GetAttribute<TestFixtureAttribute>(true) != null)
+                .SelectMany(type=>type.GetMethods())
+                .Where(methodInfo => ReflectionExtender.GetAttribute<TestAttribute>((MethodInfo) methodInfo, true) != null);
+        }
+
+        private static IEnumerable<Assembly> GetAssemblies(Assembly rootAssembly)
+        {
+            var result = new[] {rootAssembly};
+            for(var referencedAssemblies = result;
+                referencedAssemblies.Length > 0;
+                result = result.Union(referencedAssemblies).ToArray())
+            {
+                referencedAssemblies = referencedAssemblies
+                    .SelectMany(assembly => assembly.GetReferencedAssemblies())
+                    .Select(AssemblyLoad)
+                    .Distinct()
+                    .Where(assembly => !result.Contains(assembly))
+                    .ToArray();
+            }
+            return result;
+        }
+
+        private static Assembly AssemblyLoad(AssemblyName yy)
+        {
+            try
+            {
+                return AppDomain.CurrentDomain.Load(yy);
+            }
+            catch(Exception e)
+            {
+                return Assembly.GetExecutingAssembly();
+            }
+        }
+    }
+}
