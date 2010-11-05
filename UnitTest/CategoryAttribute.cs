51fa72d938fc99b1d66696fcef0e798a2c508ea3
hh <hh@c0adb3ac-85ad-b347-9a16-a660ac379d24>
2010-11-05 22:58:00 +0000

    Improvements
    git-svn-id: svn://svn.hahoyer.dyndns.org/migrep1/trunk/Framework/src/HWClassLibrary@4629 c0adb3ac-85ad-b347-9a16-a660ac379d24

--- UnitTest/CategoryAttribute.cs
+++ UnitTest/CategoryAttribute.cs
@@ -4,7 +4,7 @@ using JetBrains.Annotations;
 namespace HWClassLibrary.UnitTest
 {
     [AttributeUsage(AttributeTargets.Method)]
-    public class CategoryAttribute : Attribute
+    public sealed class CategoryAttribute : Attribute
     {
         public readonly string Name;
         public CategoryAttribute(string name) { Name = name; }
@@ -12,24 +12,24 @@ namespace HWClassLibrary.UnitTest
 
     [AttributeUsage(AttributeTargets.Method)]
     [MeansImplicitUse]
-    public class TestAttribute : Attribute
+    public sealed class TestAttribute : Attribute
     {
     }
 
     [AttributeUsage(AttributeTargets.Class)]
     [MeansImplicitUse]
-    public class TestFixtureAttribute : Attribute
+    public sealed class TestFixtureAttribute : Attribute
     {
     }
 
     [AttributeUsage(AttributeTargets.Method)]
     [MeansImplicitUse]
-    public class SetUpAttribute : Attribute
+    public abstract class SetUpAttribute : Attribute
     {
     }
 
     [AttributeUsage(AttributeTargets.Method)]
-    public class ExplicitAttribute : Attribute
+    public sealed class ExplicitAttribute : Attribute
     {
     }
 }
