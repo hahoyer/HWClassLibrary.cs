642fb2dbaf9353611aeefea4b617ee73568ba903
Harald Hoyer <hahoyer@gmail.com>
2011-09-12 09:45:47 +0200

    New: CheckedApply and functional array constructor

--- Helper/ListExtender.cs
+++ Helper/ListExtender.cs
@@ -1,6 +1,26 @@
+//     Compiler for programming language "Reni"
+//     Copyright (C) 2011 Harald Hoyer
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     
+//     Comments, bugs and suggestions to hahoyer at yahoo.de
+
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using HWClassLibrary.Debug;
 
 namespace HWClassLibrary.Helper
@@ -10,40 +30,34 @@ namespace HWClassLibrary.Helper
         public static bool AddDistinct<T>(this IList<T> a, IEnumerable<T> b, Func<T, T, bool> isEqual) { return InternalAddDistinct(a, b, isEqual); }
         public static bool AddDistinct<T>(this IList<T> a, IEnumerable<T> b, Func<T, T, T> combine) where T : class { return InternalAddDistinct(a, b, combine); }
 
-        private static bool InternalAddDistinct<T>(ICollection<T> a, IEnumerable<T> b, Func<T, T, bool> isEqual)
+        static bool InternalAddDistinct<T>(ICollection<T> a, IEnumerable<T> b, Func<T, T, bool> isEqual)
         {
             var result = false;
             foreach(var bi in b)
-            {
                 if(AddDistinct(a, bi, isEqual))
                     result = true;
-            }
             return result;
         }
 
-        private static bool InternalAddDistinct<T>(IList<T> a, IEnumerable<T> b, Func<T, T, T> combine) where T : class
+        static bool InternalAddDistinct<T>(IList<T> a, IEnumerable<T> b, Func<T, T, T> combine) where T : class
         {
             var result = false;
             foreach(var bi in b)
-            {
                 if(AddDistinct(a, bi, combine))
                     result = true;
-            }
             return result;
         }
 
-        private static bool AddDistinct<T>(ICollection<T> a, T bi, Func<T, T, bool> isEqual)
+        static bool AddDistinct<T>(ICollection<T> a, T bi, Func<T, T, bool> isEqual)
         {
             foreach(var ai in a)
-            {
                 if(isEqual(ai, bi))
                     return false;
-            }
             a.Add(bi);
             return true;
         }
 
-        private static bool AddDistinct<T>(IList<T> a, T bi, Func<T, T, T> combine) where T : class
+        static bool AddDistinct<T>(IList<T> a, T bi, Func<T, T, T> combine) where T : class
         {
             for(var i = 0; i < a.Count; i++)
             {
@@ -99,10 +113,8 @@ namespace HWClassLibrary.Helper
             if(x.Count < y.Count)
                 return false;
             for(var i = 0; i < y.Count; i++)
-            {
                 if(!Equals(x[i], y[i]))
                     return false;
-            }
             return true;
         }
 
@@ -125,6 +137,50 @@ namespace HWClassLibrary.Helper
             var xx = x.ToArray();
             Tracer.Assert(xx.Length == 1);
             return xx[0];
+        }
+
+        public static TResult CheckedApply<T, TResult>(this T target, Func<T, TResult> function)
+            where T : class
+            where TResult : class { return target == default(T) ? default(TResult) : function(target); }
+
+        public static IEnumerable<T> Array<T>(this int count, Func<int, T> getValue) { return new ArrayQuery<T>(count, getValue); }
+
+        sealed class ArrayQuery<T> : IEnumerable<T>
+        {
+            readonly int _count;
+            readonly Func<int, T> _getValue;
+            public ArrayQuery(int count, Func<int, T> getValue)
+            {
+                _count = count;
+                _getValue = getValue;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() { return ((IEnumerable<T>) this).GetEnumerator(); }
+            IEnumerator<T> IEnumerable<T>.GetEnumerator() { return new Enumerator(this); }
+
+            sealed class Enumerator : IEnumerator<T>
+            {
+                readonly ArrayQuery<T> _arrayQuery;
+                int? _index;
+                public Enumerator(ArrayQuery<T> arrayQuery) { _arrayQuery = arrayQuery; }
+
+                void IDisposable.Dispose() { }
+                bool IEnumerator.MoveNext()
+                {
+                    if(_index == null)
+                        _index = 0;
+                    else
+                        _index++;
+
+                    return _index < _arrayQuery._count;
+                }
+
+                void IEnumerator.Reset() { _index = null; }
+// ReSharper disable PossibleInvalidOperationException
+                T IEnumerator<T>.Current { get { return _arrayQuery._getValue(_index.Value); } } // should throw an exception if _index is null
+// ReSharper restore PossibleInvalidOperationException
+                object IEnumerator.Current { get { return ((IEnumerator<T>) this).Current; } }
+            }
         }
     }
 }
