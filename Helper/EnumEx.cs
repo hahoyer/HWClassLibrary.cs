b859115eb47a96d9e06893cc1d8ce68335e21a6b
S4MHH <hahoyer@gmail.com>
2012-09-11 18:03:40 +0200

    New: EnumEx-class

--- Helper/EnumEx.cs
+++ Helper/EnumEx.cs
@@ -14,40 +14,56 @@ namespace HWClassLibrary.Helper
         static EnumEx Instance(MemberInfo memberInfo) { return (EnumEx) ((FieldInfo) memberInfo).GetValue(null); }
         protected static IEnumerable<T> AllInstances<T>() { return AllInstances(typeof(T)).Cast<T>(); }
 
-        static bool IsValue(MemberInfo memberInfo, Type type)
+        static bool IsValue(MemberInfo m, Type type)
         {
-            if(memberInfo.DeclaringType != type)
+            if(m.DeclaringType != type)
                 return false;
-            if(memberInfo.MemberType != MemberTypes.Field)
+            if(m.MemberType != MemberTypes.Field)
                 return false;
-            if (memberInfo.GetAttribute<NotAEnumInstanceAttribute>(false) != null)
+            if(m.GetAttribute<NotAnEnumInstanceAttribute>(false) != null)
                 return false;
-            var fieldInfo = memberInfo as FieldInfo;
+            var fieldInfo = m as FieldInfo;
             if(fieldInfo == null)
                 return false;
-            if(fieldInfo.FieldType != type)
+            if(fieldInfo.FieldType != type && !fieldInfo.FieldType.IsSubclassOf(type))
                 return false;
             if((fieldInfo.Attributes & FieldAttributes.Static) == 0)
                 return false;
             return true;
         }
         public override string ToString() { return Tag; }
-        public string Tag { get { return AllMemberInfos(GetType()).Single(t => Instance(t) == this).Name; } }
+        public string Tag
+        {
+            get
+            {
+                for(var type = GetType(); type != null; type = type.BaseType)
+                {
+                    var instance = AllMemberInfos(type).SingleOrDefault(t => Instance(t) == this);
+                    if(instance != null)
+                        return instance.Name;
+                }
+                return null;
+            }
+        }
     }
 
     [TestFixture]
     public sealed class TestEnumEx
     {
-        sealed class MyEnum : EnumEx
+        class MyEnum : EnumEx
         {
             public static readonly MyEnum X1 = new MyEnum();
             public static readonly MyEnum X2 = new MyEnum();
             public static readonly MyEnum X3 = new MyEnum();
-            public static readonly MyEnum X4 = new MyEnum();
-            [NotAEnumInstance]
+            public static readonly MyEnumDerived X4 = new MyEnumDerived();
+            static readonly MyEnum XPrivate = new MyEnum();
+            [NotAnEnumInstance]
             public static readonly MyEnum Xclude = new MyEnum();
             public static IEnumerable<MyEnum> All { get { return AllInstances<MyEnum>(); } }
         }
+
+        class MyEnumDerived : MyEnum
+        {}
 
         [Test]
         public void Test()
@@ -55,10 +71,12 @@ namespace HWClassLibrary.Helper
             var all = MyEnum.All.ToArray();
             Tracer.Assert(all.Length == 4);
 
+            Tracer.Assert(MyEnum.X4.Tag == "X4");
+            Tracer.Assert(new MyEnum().Tag == null);
             Tracer.Assert(MyEnum.X1.Tag == "X1");
         }
     }
 
-    sealed class NotAEnumInstanceAttribute : Attribute
+    sealed class NotAnEnumInstanceAttribute : Attribute
     {}
 }
