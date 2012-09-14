5164cd53e43e0a32db2990a9a39602358e3a5dc6
Harald Hoyer <hahoyer@gmail.com>
2012-09-14 08:59:55 +0200

    New: Enum class

--- Helper/EnumEx.cs
+++ Helper/EnumEx.cs
@@ -1,3 +1,25 @@
+#region Copyright (C) 2012
+
+//     Project HWClassLibrary
+//     Copyright (C) 2012 - 2012 Harald Hoyer
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     
+//     Comments, bugs and suggestions to hahoyer at yahoo.de
+
+#endregion
+
 using System.Linq;
 using System.Collections.Generic;
 using System;
@@ -7,7 +29,7 @@ using HWClassLibrary.UnitTest;
 
 namespace HWClassLibrary.Helper
 {
-    abstract class EnumEx
+    public abstract class EnumEx
     {
         static IEnumerable<MemberInfo> AllMemberInfos(Type type) { return type.GetMembers().Where(memberInfo => IsValue(memberInfo, type)); }
         static IEnumerable<EnumEx> AllInstances(Type type) { return AllMemberInfos(type).Select(Instance); }
