42154971ccc12b7cb5f778da15618ae4710e66b0
hahoyer <hahoyer@gmail.com>
2014-01-14 10:26:18 +0100

    #12: New: omitZeros, + test

---
+++ src/hw.nuget/Tests/Helper/Core/HelperCore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using hw.Debug;
+using hw.Helper;
+using hw.UnitTest;
+
+namespace hw.Tests.Helper.Core
+{
+    [TestFixture]
+    public static class DateTimeExtender
+    {
+        [Test]
+        public static void Format3DigitsSmart()
+        {
+            var t1 = TimeSpan.FromMinutes(3.12);
+            var t2 = TimeSpan.FromMinutes(3);
+            var t3 = TimeSpan.FromMinutes(3.0012);
+
+            Tracer.Assert(t1.Format3Digits() == t1.Format3Digits(omitZeros:true), () => t1.Format3Digits() + " != " + t1.Format3Digits(omitZeros:true));
+            Tracer.Assert(t2.Format3Digits() != t2.Format3Digits(omitZeros: true) && t2.Format3Digits(omitZeros: true) == "3m", () => t2.Format3Digits() + " != " + t2.Format3Digits(omitZeros: true));
+            Tracer.Assert(t3.Format3Digits() != t3.Format3Digits(omitZeros:true), () => t3.Format3Digits() + " != " + t3.Format3Digits(omitZeros:true));
+        }
+    }
+}
