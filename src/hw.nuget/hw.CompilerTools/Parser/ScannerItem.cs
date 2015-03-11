941ca2d09b47af10f3ff4ef110494be87f946d03
hahoyer <hahoyer@gmail.com>
2015-03-11 23:35:12 +0100

    Refactor: Calles separated

--- src/hw.nuget/hw.CompilerTools/Parser/ScannerItem.cs
+++ src/hw.nuget/hw.CompilerTools/Parser/ScannerItem.cs
@@ -1,11 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using hw.Helper;
 using System.Linq;
-using hw.Debug;
-using hw.Scanner;
-using JetBrains.Annotations;
 
 namespace hw.Parser
 {
@@ -25,55 +20,5 @@ namespace hw.Parser
         {
             return Type.Create(left, Token, right);
         }
-    }
-
-    [DebuggerDisplay("{NodeDump}")]
-    public sealed class Token
-    {
-        public readonly WhiteSpaceToken[] PrecededWith;
-        public readonly SourcePart Characters;
-
-        public Token(SourcePart characters, WhiteSpaceToken[] precededWith)
-        {
-            Characters = characters;
-            PrecededWith = precededWith ?? new WhiteSpaceToken[0];
-            AssertValid();
-        }
-
-        void AssertValid()
-        {
-            for(var i = 1; i < PrecededWith.Length; i++)
-                Tracer.Assert
-                    (PrecededWith[i - 1].Characters.End.Equals(PrecededWith[i].Characters.Start));
-            var l = PrecededWith.LastOrDefault();
-            if(l == null)
-                return;
-            Tracer.Assert(l.Characters.End.Equals(Characters.Start));
-        }
-
-        public SourcePosn Start { get { return SourcePart.Start; } }
-        public SourcePart SourcePart
-        {
-            get { return (Characters + PrecededWith.Select(item => item.Characters).Aggregate()); }
-        }
-
-        public string Name { get { return Characters.Name; } }
-
-        [UsedImplicitly]
-        public string NodeDump { get { return Name; } }
-    }
-
-    [DebuggerDisplay("{NodeDump}")]
-    public sealed class WhiteSpaceToken
-    {
-        public readonly int Index;
-        public readonly SourcePart Characters;
-        public WhiteSpaceToken(int index, SourcePart characters)
-        {
-            Index = index;
-            Characters = characters;
-        }
-        [UsedImplicitly]
-        public string NodeDump { get { return Characters.NodeDump + "." + Index + "i"; } }
     }
 }
