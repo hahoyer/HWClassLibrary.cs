56b8a96d99bf8de9abef4b2f6f87decf7b6d9ebe
hahoyer <hahoyer@gmail.com>
2015-04-05 11:20:52 +0200

    Refactor

--- src/hw.nuget/hw.CompilerTools/Parser/IToken.cs
+++ src/hw.nuget/hw.CompilerTools/Parser/IToken.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using hw.Debug;
 using hw.Scanner;
 
 namespace hw.Parser
 {
     public interface IToken
     {
-        SourcePosn Start { get; }
+        [DisableDump]
         SourcePart SourcePart { get; }
+        [DisableDump]
         string Id { get; }
+        [DisableDump]
         WhiteSpaceToken[] PrecededWith { get; }
+        [DisableDump]
         SourcePart Characters { get; }
     }
 }
