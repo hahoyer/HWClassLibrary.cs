0d6ab50678a82d23d4154c4e5b0ba9b50bf4911c
hahoyer <hahoyer@gmail.com>
2016-03-21 09:56:44 +0100

    New: Nice shortcuts to use in debugger (2nd trial)

--- src/hw.nuget/hw.Helper.Core/DebugFormatter/Dumpable.cs
+++ src/hw.nuget/hw.Helper.Core/DebugFormatter/Dumpable.cs
@@ -29,6 +29,11 @@ namespace hw.DebugFormatter
         [UsedImplicitly]
         public string DebuggerDumpString { get { return DebuggerDump().Replace("\n", "\r\n"); } }
 
+        [DisableDump]
+        [UsedImplicitly]
+        public string D => DebuggerDumpString;
+        public void T() => Tracer.Line(DebuggerDumpString);
+
         /// <summary>
         ///     Method dump with break,
         /// </summary>
