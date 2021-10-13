b68cf432b5cbe11641b7f29ffa75c9d8870e21ae
hahoyer-21 <hahoyer@gmail.com>
2021-10-13 11:09:10 +0200

    version 21.0.1

--- src/hw.nuget/hw.Helper.Core/DebugFormatter/Tracer.cs
+++ src/hw.nuget/hw.Helper.Core/DebugFormatter/Tracer.cs
@@ -329,7 +329,7 @@ namespace hw.DebugFormatter
         /// <param name="target"> the object to dump </param>
         /// <returns> </returns>
         public static string Dump(object target) => Dumper.Dump(target);
-
+        public static string LogDump(this object target) => Dumper.Dump(target);
 
         /// <summary>
         ///     generic dump function by use of reflection
