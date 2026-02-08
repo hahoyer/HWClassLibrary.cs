a412c19f99db11dc71dd4d6789875c89d44cc056
Harald Herbert Hoyer <hahoyer@gmail.com>
2026-02-08 13:21:00 +0100

    admin

--- src/hw.Helper.Core/hw/DebugFormatter/DisableDumpAttribute.cs
+++ src/hw.Helper.Core/hw/DebugFormatter/DisableDumpAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace hw.DebugFormatter;
 
+/// <summary>
+///     Used to control dump of data element.
+///     Inhibit dump of this member, even if it's not private
+/// </summary>
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 public sealed class DisableDumpAttribute : DumpEnabledAttribute
 {
--- src/hw.Helper.Core/hw/DebugFormatter/EnableDumpAttribute.cs
+++ src/hw.Helper.Core/hw/DebugFormatter/EnableDumpAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace hw.DebugFormatter;
 
+/// <summary>
+///     Used to control dump of data element.
+///     Enable dump of this member, even if it's private
+/// </summary>
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
 [MeansImplicitUse]
 [PublicAPI]
