a5edb56613f290e3364945a201cb73504dcae0df
hahoyer <hahoyer@gmail.com>
2026-04-03 07:53:42 +0200

    new: File and parsing support for long files

---
+++ src/hw.CompilerTools/hw/Scanner64/FileSourceProvider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using hw.Helper;
+
+// ReSharper disable CheckNamespace
+
+namespace hw.Scanner64;
+
+public sealed class FileSourceProvider : ISourceProvider, ITextProvider
+{
+    sealed class Enumerator(FileSourceProvider parent) : IEnumerator<char>
+    {
+        long Cursor;
+        void IDisposable.Dispose() { }
+
+        object IEnumerator.Current => parent.File[Cursor];
+
+        bool IEnumerator.MoveNext()
+        {
+            if(Cursor >= parent.File.Parent.Size)
+                return false;
+            Cursor++;
+            return true;
+        }
+
+        void IEnumerator.Reset() => Cursor = 0;
+
+        char IEnumerator<char>.Current => parent.File[Cursor];
+    }
+
+    readonly SmbFileBuffer File;
+
+    public FileSourceProvider(SmbFile file, int? blockSize = null)
+        => File = new(file, blockSize);
+
+    IEnumerator IEnumerable.GetEnumerator() => new Enumerator(this);
+    IEnumerator<char> IEnumerable<char>.GetEnumerator() => new Enumerator(this);
+
+    ITextProvider ISourceProvider.Data => this;
+
+    string ISourceProvider.Identifier => File.Parent.FullName;
+
+    bool ISourceProvider.IsPersistent => true;
+    long ISourceProvider.Length => File.Parent.Size;
+    string ITextProvider.this[Range64 range] => File[range];
+    char ITextProvider.this[Index64 index] => File[index];
+}
---
+++ src/hw.CompilerTools/hw/Scanner64/ISourceProvider.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable CheckNamespace
+
+using hw.Helper;
+
+namespace hw.Scanner64;
+
+public interface ISourceProvider
+{
+    ITextProvider Data { get; }
+    bool IsPersistent { get; }
+    long Length { get; }
+    string? Identifier { get; }
+}
+
+public interface IMultiSourceProvider
+{
+    SourcePosition Position(long position, bool isEnd);
+}
+
+public interface ITextProvider: IEnumerable<char>
+{
+    string this[Range64 range] { get; }
+    char this[Index64 index] { get; }
+}
+
