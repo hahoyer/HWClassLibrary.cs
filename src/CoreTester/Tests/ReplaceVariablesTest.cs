9eaa7902fc9e44102a73e3735357e275292bb137
hahoyer <hahoyer@gmail.com>
2026-04-03 07:45:54 +0200

    test

---
+++ src/CoreTester/Tests/ReplaceVariablesTest.cs
@@ -0,0 +1,20 @@
+﻿namespace CoreTester.Tests;
+
+[UnitTest]
+public class ReplaceVariablesTest
+{
+    [UnitTest]
+    public void SimpleTest()
+    {
+        var text = "d {Name} {Age} k";
+        var found = text.ReplaceVariables(2);
+
+        var expected = "";
+
+        (found == expected)
+            .Assert
+            (() =>
+                "\nXpctd: " + expected + "\nFound: " + found + "\nText : " + text
+            );
+    }
+}
